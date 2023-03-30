3dc218809ef1f50d7533d2aaf8e52525efe2fac8
eriosta <eri@sienaspace.com>
2023-03-30 10:44:28 -0500

    Add segs

--- mri/dataset.py
+++ mri/dataset.py
@@ -20,10 +20,19 @@ class MRIImageFolder:
             patient_id = row["ID"]
             class_label = row["EDSS_cat"]
             patient_dir = os.path.join(self.mri_dir, f"Patient-{patient_id}")
-            for sequence in ["T1", "T2", "Flair"]:
-                input_path = os.path.join(patient_dir, f"{patient_id}-{sequence}.nii")
-                output_path = os.path.join(self.output_dir, f"Class {class_label}", f"{patient_id}-{sequence}.nii")
-                copyfile(input_path, output_path)
-                processed_patients += 1
-                if processed_patients % 10 == 0:
-                    print(f"Processed {processed_patients} of {total_patients} patients.")
+            for filename in os.listdir(patient_dir):
+                if "-LesionSeg-" in filename:
+                    sequence = filename.split("-LesionSeg-")[-1].replace(".nii", "")
+                else:
+                    sequence = None
+                    for s in ["T1", "T2", "Flair"]:
+                        if s in filename:
+                            sequence = s
+                            break
+                if sequence:
+                    input_path = os.path.join(patient_dir, filename)
+                    output_path = os.path.join(self.output_dir, f"Class {class_label}", f"{patient_id}-{sequence}.nii")
+                    copyfile(input_path, output_path)
+                    processed_patients += 1
+                    if processed_patients % 10 == 0:
+                        print(f"Processed {processed_patients} of {total_patients} patients.")
