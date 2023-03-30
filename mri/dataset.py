2c90d6211b8d8814014f308a3742bf518d754cda
eriosta <eri@sienaspace.com>
2023-03-30 10:52:37 -0500

    Mod

--- mri/dataset.py
+++ mri/dataset.py
@@ -1,16 +1,20 @@
 import os
 import pandas as pd
 from shutil import copyfile
+from torchvision.datasets import ImageFolder
 
 class MRIImageFolder:
     def __init__(self, csv_path, mri_dir, output_dir):
         self.csv_path = csv_path
         self.mri_dir = mri_dir
         self.output_dir = output_dir
+        self.mri_imagefolder = None
+        self.mask_imagefolder = None
 
     def create_directories(self):
         for class_name in ["Class 0", "Class 1", "Class 2"]:
-            os.makedirs(os.path.join(self.output_dir, class_name), exist_ok=True)
+            os.makedirs(os.path.join(self.output_dir, "MRI", class_name), exist_ok=True)
+            os.makedirs(os.path.join(self.output_dir, "Mask", class_name), exist_ok=True)
 
     def convert_to_imagefolder(self):
         df = pd.read_csv(self.csv_path)
@@ -20,19 +24,19 @@ class MRIImageFolder:
             patient_id = row["ID"]
             class_label = row["EDSS_cat"]
             patient_dir = os.path.join(self.mri_dir, f"Patient-{patient_id}")
-            for filename in os.listdir(patient_dir):
-                if "-LesionSeg-" in filename:
-                    sequence = filename.split("-LesionSeg-")[-1].replace(".nii", "")
-                else:
-                    sequence = None
-                    for s in ["T1", "T2", "Flair"]:
-                        if s in filename:
-                            sequence = s
-                            break
-                if sequence:
-                    input_path = os.path.join(patient_dir, filename)
-                    output_path = os.path.join(self.output_dir, f"Class {class_label}", f"{patient_id}-{sequence}.nii")
-                    copyfile(input_path, output_path)
-                    processed_patients += 1
-                    if processed_patients % 10 == 0:
-                        print(f"Processed {processed_patients} of {total_patients} patients.")
+            num_files = 0
+            for sequence in ["T1", "T2", "Flair"]:
+                input_path = os.path.join(patient_dir, f"{patient_id}-{sequence}.nii")
+                output_path = os.path.join(self.output_dir, "MRI", f"Class {class_label}", f"{patient_id}-{sequence}.nii")
+                copyfile(input_path, output_path)
+                num_files += 1
+            for sequence in ["T1", "T2", "Flair"]:
+                input_path = os.path.join(patient_dir, f"{patient_id}-LesionSeg-{sequence}.nii")
+                output_path = os.path.join(self.output_dir, "Mask", f"Class {class_label}", f"{patient_id}-LesionSeg-{sequence}.nii")
+                copyfile(input_path, output_path)
+                num_files += 1
+            processed_patients += 1
+            print(f"Processed patient {patient_id} ({num_files} files). {processed_patients} of {total_patients} patients.")
+        # Create ImageFolders for the MRI scans and lesion segmentation masks
+        self.mri_imagefolder = ImageFolder(os.path.join(self.output_dir, "MRI"))
+        self.mask_imagefolder = ImageFolder(os.path.join(self.output_dir, "Mask"))
