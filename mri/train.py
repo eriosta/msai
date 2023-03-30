399e2f6bc86d361b3d7b3f29820d83eb7d4f337f
eriosta <eri@sienaspace.com>
2023-03-30 12:22:58 -0500

    Training script

--- mri/train.py
+++ mri/train.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import nibabel as nib
+import glob
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
@@ -20,21 +21,41 @@ class ViTTrainer:
         self.history = None
 
     def load_nii_data(self):
-        nii_files = os.listdir(self.nii_dir)
+        mri_folder = os.path.join(self.nii_dir, 'MRI')
+        mask_folder = os.path.join(self.nii_dir, 'Mask')
         data = []
         masks = []
-        for nii_file in nii_files:
-            if nii_file.endswith('.nii'):
-                nii_path = os.path.join(self.nii_dir, nii_file)
-                nii_data = nib.load(nii_path).get_fdata()
-                slices = np.rollaxis(nii_data, 2)  # Roll the axis to get a list of slices
-                data.extend(slices)
+        mri_paths = []
+        mask_paths = []
 
-                # Load the corresponding segmentation mask for each slice
-                mask_path = os.path.splitext(nii_path)[0] + '_mask.nii'
-                mask_data = nib.load(mask_path).get_fdata()
-                mask_slices = np.rollaxis(mask_data, 2)
-                masks.extend(mask_slices)
+        for class_dir in os.listdir(os.path.join(self.nii_dir, "MRI")):
+            for sequence in ["T1", "T2", "Flair"]:
+                # Find all MRI files for the current class and sequence
+                mri_files = sorted(glob.glob(os.path.join(mri_folder, class_dir, f"*-{sequence}.nii")))
+
+                # Find all corresponding mask files
+                mask_files = sorted(glob.glob(os.path.join(mask_folder, class_dir, f"*-LesionSeg-{sequence}.nii")))
+
+                # Load MRI data and corresponding masks
+                for mri_path, mask_path in zip(mri_files, mask_files):
+                    mri_data = nib.load(mri_path).get_fdata()
+                    slices = np.rollaxis(mri_data, 2)  # Roll the axis to get a list of slices
+                    data.extend(slices)
+
+                    mask_data = nib.load(mask_path).get_fdata()
+                    mask_slices = np.rollaxis(mask_data, 2)
+                    masks.extend(mask_slices)
+
+                    # Get the file names without the patient ID
+                    mri_file_name = os.path.basename(mri_path)
+                    mask_file_name = os.path.basename(mask_path)
+                    mri_file_name = mri_file_name.split('-', 1)[1]
+                    mask_file_name = mask_file_name.split('-', 1)[1]
+
+                    # Save the file paths without the patient ID
+                    mri_paths.append(os.path.join(mri_folder, class_dir, mri_file_name))
+                    mask_paths.append(os.path.join(mask_folder, class_dir, mask_file_name))
+
         self.data = np.array(data)
         self.masks = np.array(masks)
 
@@ -49,27 +70,19 @@ class ViTTrainer:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(data_rescaled, masks_rescaled, test_size=0.2, random_state=42)
         self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X_train, self.y_train, test_size=0.2, random_state=42)
 
-    def load_model(self):
-        # Load ViT model and pre-trained weights
-        self.model = vit.ViT(image_size=self.img_size, classes=self.classes, include_top=self.include_top, pretrained=self.pretrained)
+    def load_model(self, model_path=None, weights_path=None):
+            # Load ViT model and pre-trained weights
+            self.model = vit.ViT(image_size=self.img_size, classes=self.classes, include_top=self.include_top, pretrained=self.pretrained)
 
-    def add_attention_mask(self):
-        attention_mask = tf.keras.layers.Input(shape=(self.img_size, self.img_size, 1))
-        x = tf.keras.layers.Concatenate()([self.model.output, attention_mask])
-        self.model = tf.keras.Model(inputs=[self.model.input, attention_mask], outputs=x)
+            # Load saved weights if provided
+            if weights_path:
+                self.model.load_weights(weights_path)
 
-    def train_model(self, epochs=10):
-        # Fine-tune ViT model on training set
-        self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        self.history = self.model.fit([self.X_train, self.y_train], self.y_train, epochs=epochs, validation_data=([self.X_val, self.y_val], self.y_val))
-        
-        # Save model and weights
-        model_dir = os.path.join(self.nii_dir, 'models')
-        os.makedirs(model_dir, exist_ok=True)
-        model_path = os.path.join(model_dir, f'vit_model_{self.img_size}.h5')
-        self.model.save(model_path)
-        
-        weights_dir = os.path.join(self.nii_dir, 'weights')
-        os.makedirs(weights_dir, exist_ok=True)
-        weights_path = os.path.join(weights_dir, f'vit_weights_{self.img_size}.h5')
-        self.model.save_weights(weights_path)
+            # Load saved model if provided
+            if model_path:
+                self.model = tf.keras.models.load_model(model_path, compile=False)
+
+            # Add attention mask to ViT model
+            attention_mask = tf.keras.layers.Input(shape=(self.img_size, self.img_size, 1))
+            x = tf.keras.layers.Concatenate()([self.model.output, attention_mask])
+            self.model = tf.keras.Model(inputs=[self.model.input, attention_mask], outputs=x)
